8f4fcc57bc6751dfd55a5f7e1cfea7132f511d21
dimasrizqi <dimasrizqi3128@yahoo.co.id>
2020-07-14 13:49:37 +0700

    Update mixing.py

--- mixing.py
+++ mixing.py
@@ -13,21 +13,29 @@ import random
 db = mysql.connector.connect(host="10.10.1.254",port="3333", user="admin_iotfutami", passwd="futami17", db="admin_iotfutami") # replace password with your password
 cur = db.cursor()
 
-def id_sensor():
-        var1 = "MIXING"
-        return var1
+def sensorSterilization():
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        if not GPIO.input(18):
+                numr = random.randint(200, 225)
+                val = numr
+        else:
+                numr = random.randint(75, 100)
+                val = numr
+        GPIO.cleanup()
+        return val
 
 def sensorProduction():
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         if not GPIO.input(26):
                 numr = random.randint(200, 225)
-                pascals = numr
+                val = numr
         else:
                 numr = random.randint(75, 100)
-                pascals = numr
+                val = numr
         GPIO.cleanup()
-        return pascals
+        return val
 
 def sensorCIP():
         GPIO.setmode(GPIO.BCM)
@@ -35,41 +43,50 @@ def sensorCIP():
         numr2 = random.randint(200, 225)
         if not GPIO.input(25):
                 numr = random.randint(200, 225)
-                pascals = numr
+                val = numr
         else:
                 numr = random.randint(75, 100)
-                pascals = numr
+                val = numr
         GPIO.cleanup()
-        return pascals
+        return val
 
-def note():
-        notenya = "ini test input dari raspbery"
-        return notenya
+def sensorTank1():
+        numr = random.randint(8000, 12000)
+        val = numr
+        return val
+
+def sensorTank2():
+        numr = random.randint(8000, 12000)
+        val = numr
+        return val
 
 channels1 = "1"
 channels2 = "2"
 channels3 = "3"
 channels4 = "4"
 channels5 = "5"
-id_sensors = id_sensor()
+id_sensors = "MIXING"
 sensorCIPs = sensorCIP()
 sensorProductions = sensorProduction()
-notes = note()
+sensorSterilizations = sensorSterilization()
+sensorTank1s = sensorTank1()
+sensorTank2s = sensorTank2()
+notes = "ini test input dari raspbery"
 
 sql1 = ("""INSERT INTO fyp (id_sensor,channel,sensor_read,note) VALUES (%s,%s,%s,%s)""", (id_sensors, channels1, sensorProductions, notes))
 sql2 = ("""INSERT INTO fyp (id_sensor,channel,sensor_read,note) VALUES (%s,%s,%s,%s)""", (id_sensors, channels2, sensorCIPs, notes))
-sql3 = ("""INSERT INTO fyp (id_sensor,channel,sensor_read,note) VALUES (%s,%s,%s,%s)""", (id_sensors, channels3, sensorProductions, notes))
-sql4 = ("""INSERT INTO fyp (id_sensor,channel,sensor_read,note) VALUES (%s,%s,%s,%s)""", (id_sensors, channels4, sensorProductions, notes))
-sql5 = ("""INSERT INTO fyp (id_sensor,channel,sensor_read,note) VALUES (%s,%s,%s,%s)""", (id_sensors, channels5, sensorProductions, notes))
+sql3 = ("""INSERT INTO fyp (id_sensor,channel,sensor_read,note) VALUES (%s,%s,%s,%s)""", (id_sensors, channels3, sensorSterilizations, notes))
+sql4 = ("""INSERT INTO fyp (id_sensor,channel,sensor_read,note) VALUES (%s,%s,%s,%s)""", (id_sensors, channels4, sensorTank1s, notes))
+sql5 = ("""INSERT INTO fyp (id_sensor,channel,sensor_read,note) VALUES (%s,%s,%s,%s)""", (id_sensors, channels5, sensorTank2s, notes))
 try:
-    print "Writing to the database..."
+#    print "Writing to the database..."
     cur.execute(*sql1)
     cur.execute(*sql2)
     cur.execute(*sql3)
     cur.execute(*sql4)
     cur.execute(*sql5)
     db.commit()
-    print "Write complete"
+#    print "Write complete"
 
 except:
     db.rollback()
@@ -77,8 +94,3 @@ except:
 
 cur.close()
 db.close()
-
-#print secs
-#print temperature
-#print pressure
-#print humidity
